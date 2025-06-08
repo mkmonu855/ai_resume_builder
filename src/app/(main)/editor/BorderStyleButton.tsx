208674d92a0016ec959c9cfdde7fde35beeca763
mkmonu855 <mkmonu855@gmail.com>
2025-06-08 12:34:48 +0530

    Changes in editors part

--- src/app/(main)/editor/BorderStyleButton.tsx
+++ src/app/(main)/editor/BorderStyleButton.tsx
@@ -1,8 +1,5 @@
 import { Button } from "@/components/ui/button";
-import usePremiumModal from "@/hooks/usePremiumModal";
-import { canUseCustomizations } from "@/lib/permissions";
 import { Circle, Square, Squircle } from "lucide-react";
-import { useSubscriptionLevel } from "../SubscriptionLevelProvider";
 
 export const BorderStyles = {
   SQUARE: "square",
@@ -21,16 +18,7 @@ export default function BorderStyleButton({
   borderStyle,
   onChange,
 }: BorderStyleButtonProps) {
-  const subscriptionLevel = useSubscriptionLevel();
-
-  const premiumModal = usePremiumModal();
-
   function handleClick() {
-    if (!canUseCustomizations(subscriptionLevel)) {
-      premiumModal.setOpen(true);
-      return;
-    }
-
     const currentIndex = borderStyle ? borderStyles.indexOf(borderStyle) : 0;
     const nextIndex = (currentIndex + 1) % borderStyles.length;
     onChange(borderStyles[nextIndex]);
--- src/app/(main)/editor/ColorPicker.tsx
+++ src/app/(main)/editor/ColorPicker.tsx
@@ -4,12 +4,9 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import usePremiumModal from "@/hooks/usePremiumModal";
-import { canUseCustomizations } from "@/lib/permissions";
 import { PaletteIcon } from "lucide-react";
 import { useState } from "react";
 import { Color, ColorChangeHandler, TwitterPicker } from "react-color";
-import { useSubscriptionLevel } from "../SubscriptionLevelProvider";
 
 interface ColorPickerProps {
   color: Color | undefined;
@@ -17,10 +14,6 @@ interface ColorPickerProps {
 }
 
 export default function ColorPicker({ color, onChange }: ColorPickerProps) {
-  const subscriptionLevel = useSubscriptionLevel();
-
-  const premiumModal = usePremiumModal();
-
   const [showPopover, setShowPopover] = useState(false);
 
   return (
@@ -30,13 +23,7 @@ export default function ColorPicker({ color, onChange }: ColorPickerProps) {
           variant="outline"
           size="icon"
           title="Change resume color"
-          onClick={() => {
-            if (!canUseCustomizations(subscriptionLevel)) {
-              premiumModal.setOpen(true);
-              return;
-            }
-            setShowPopover(true);
-          }}
+          onClick={() => setShowPopover(true)}
         >
           <PaletteIcon className="size-5" />
         </Button>
